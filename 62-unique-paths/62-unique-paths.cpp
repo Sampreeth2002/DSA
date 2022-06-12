82bcf73e6a037051d1779eec4c5df6569ffe4829
Sampreeth Miriyala <61119309+Sampreeth2002@users.noreply.github.com>
2022-06-12 21:11:19 +0530

    Time: 2 ms (47.67%), Space: 6.1 MB (56.71%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -21,8 +21,30 @@ public:
     
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return noOfWays(m-1,n-1,dp);
-         // dp[m-1][n-1];
+        vector<int> prev (n,0);
+        for(int i=0;i<m;i++)
+        {
+            vector<int> curr(n,0);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                curr[j]=1;
+                continue;
+            }
+                int up = 0;
+                int down = 0;
+                if(i>0)
+                {
+                    up = prev[j];
+                }
+                if(j>0)
+                {
+                    down = curr[j-1]; 
+                }
+                curr[j] = up+down;
+            }
+            prev = curr;
+        }
+        
+        return prev[n-1];
     }
 };
