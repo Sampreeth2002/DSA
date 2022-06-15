50784946bb279f141c12b825afa5b01491f37c3d
Sampreeth Miriyala <61119309+Sampreeth2002@users.noreply.github.com>
2022-06-15 20:43:53 +0530

    Added solution - LeetHub

---
+++ 3. Rearrange a linked list - GFG/3.-rearrange-a-linked-list.cpp
@@ -0,0 +1,116 @@
+// { Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+ // } Driver Code Ends
+/* Node structure
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution
+{
+    public:
+    void rearrangeEvenOdd(Node *head)
+    {
+      
+      if(head==NULL) return ;
+      
+      Node* odd = head;
+      Node* even = head->next;
+      Node* evenFirst = head->next;
+      
+      while(1)
+      {
+          if(!odd || !even || !(even->next))
+          {
+              odd->next = evenFirst;
+              break;
+          }
+          
+          odd->next = even->next;
+          odd = even->next;
+          
+          if(odd->next==NULL)
+          {
+              even->next = NULL;
+              odd->next = evenFirst;
+              break;
+          }
+          
+          even->next = odd->next;
+          even = odd->next;
+      }
+      
+      
+      
+    }
+};
+
+
+// { Driver Code Starts.
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,i,n,l;
+
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL;
+        struct Node *temp = head;
+        cin>>n;
+        for(i=1; i<=n; i++)
+        {
+            cin>>l;
+
+            if (head == NULL)
+            {   
+                head = temp = new Node(l); 
+                
+            }
+            else
+            {  temp->next = new Node(l);
+               temp = temp->next;
+            }
+        }
+        Solution ob;
+        ob.rearrangeEvenOdd(head);
+        while(head != NULL)
+        {
+            printf("%d ", head->data);
+            head = head->next;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+  // } Driver Code Ends
