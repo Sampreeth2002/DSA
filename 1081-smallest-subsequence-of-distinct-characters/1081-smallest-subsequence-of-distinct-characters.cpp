9b83e981caeb4c8d4074e7ce2381f4d82cd0031c
Sampreeth Miriyala <61119309+Sampreeth2002@users.noreply.github.com>
2022-08-24 21:51:52 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (69.72%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     string smallestSubsequence(string s) {
-        string ans;
-        unordered_map<char,int> mp;
-        for(auto it:s)mp[it]++;
+                vector<int> last_occurence(26, -1);    
+        for(int i=0; i<s.length(); i++) {
+            last_occurence[s[i]-'a'] = i;
+        }
         
+        vector<bool> visited(26, false);
         stack<char> st;
         
-        unordered_set<char> vis;
-        
-        int n = s.size();
-        for(int i = 0;i<n;i++)
-        {
-            mp[s[i]]--;
-            if(vis.find(s[i])!=vis.end())continue;
-            else
-            {
-                while(st.size() and st.top()>=s[i] and mp[st.top()]>0)
-                {
-                    vis.erase(st.top());
-                    st.pop();
-                }
-                st.push(s[i]);
-                vis.insert(s[i]);
-            }
+        for(int i=0; i<s.length(); i++) {
+            if(visited[s[i]-'a']) continue; // if element is already present
+            while(!st.empty() && last_occurence[st.top()-'a'] > i /* this element is present ahead */ && st.top() > s[i]) {
+                visited[st.top()-'a'] = false;
+                st.pop();
+            }  
+            
+            visited[s[i]-'a'] = true;
+            st.push(s[i]);
         }
         
-        while(st.size())
-        {
-            ans.push_back(st.top());
+        string res = "";
+        while(!st.empty()) {
+            res.push_back(st.top());
             st.pop();
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        reverse(res.begin(), res.end());
+        
+        return res;
     
     }
 };
